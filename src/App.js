813814f11cf79cd4b9357c5a60e607aea99ec8f2
Christopher Song <chris4728.song2001@gmail.com>
2021-03-24 16:35:20 -0500

    Functionality for adding/removing courses & course sections

--- src/App.js
+++ src/App.js
@@ -1,102 +1,215 @@
 // import logo from './logo.svg';
 import React, { Component } from 'react';
-import { courseIdMap_sm, courseIdMap_lg, groupsSmall, groupsLarge } from './SampleData';
+import { courses_sm, courses_lg } from './SampleData';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 class Input extends Component {
   state = { 
-    numCourses: 0,
-    groups: [],
-    courseIdMap: []
+    currentId: 3,
+    courses: [
+      {
+        id: 1, 
+        title: "Course 1",
+        times: {
+          fall: [{start: null, end: null}],
+          winter: [{start: null, end: null}],
+          spring: [{start: null, end: null}]
+        },
+        series: false,
+        prereq: null
+      }, 
+      {
+        id: 2, 
+        title: "Course 2", 
+        times: {
+          fall: [{start: null, end: null}],
+          winter: [{start: null, end: null}],
+          spring: [{start: null, end: null}]
+        },
+        series: false,
+        prereq: null
+      }
+    ]
   }
 
-  handleCourse = () => {
-    console.log('Handle Course');
+  // Find course index in internal list, based on its Id number
+  findCourse = (courseId) => {
+    let courses = [...this.state.courses];
+
+    for (var i in courses) {
+      if (courses[i].id === courseId) {
+        return i;
+      };
+    };
+
+    return -1;
   }
 
+  // Add a new course 
+  addCourse = () => {
+    this.setState(oldState => ({
+      currentId: oldState.currentId + 1,
+      courses: [...oldState.courses, 
+                {
+                  id: oldState.currentId, 
+                  title: `Course ${oldState.currentId}`, 
+                  times: {
+                    fall: [{start: null, end: null}],
+                    winter: [{start: null, end: null}],
+                    spring: [{start: null, end: null}]
+                  },
+                  series: false,
+                  prereq: null
+                }]
+    }));
+  };
+
+  // Remove a specified course
+  removeCourse = (courseId) => {
+    this.setState(oldState => ({
+      courses: oldState.courses.filter((course) => course.id !== courseId)
+    }));
+  };
+
+  // Add a time slot for a course (ex. 9am and 11am sections)
+  addTime = (courseId, quarter) => {
+    let index = this.findCourse(courseId);
+
+    if (index === -1) {
+      return;
+    }
+
+    let courses = [...this.state.courses];
+    courses[index].times[quarter].push({start: null, end: null});
+    this.setState({ courses });
+  };
+
+  // Remove a time slot for a course
+  removeTime = (courseId, quarter) => {
+    let index = this.findCourse(courseId);
+
+    if (index === -1) {
+      return;
+    }
+
+    let courses = [...this.state.courses];
+    if (courses[index].times[quarter].length === 1) {
+      alert('Error: You are attempting to remove the only time slot.');
+      return;
+    };
+
+    courses[index].times[quarter].pop();
+    this.setState({ courses });
+  };
+
+  // TODO
   handleSeries = () => {
     console.log('Handle Series');
   }
 
+  // Render method
   render() { 
     return ( 
       <div className="m-bottom">
-        {groupsSmall.map((group, index) => (
-          <div key={index} className="form-row m-bottom-sm">
-            {group.map((course, index) => (
-              <div key={index} className="form-group col-md-3">
-                <div className="input-group m-bottom">
-                  <input type="text" className="form-control" placeholder="Course Title" />
-                </div>
+        <div className="form-row m-bottom-sm">
+          {this.state.courses.map((course, index) => (
+            <div key={index} className="form-group col-md-3">
 
-                <label>Prerequisites:</label>
-                <div className="input-group">
-                  <div className="form-check">
-                    <input className="form-check-input" type="checkbox" />
-                    <label className="form-check-label">None</label>
-                  </div>
-                </div>
-                <div className="input-group m-bottom">
-                    {courseIdMap_lg.map((course) => (
-                      <div className="form-check m-right">
-                        <input className="form-check-input" type="checkbox" />
-                        <label className="form-check-label">{course.title}</label>
-                      </div>
-                    ))}
-                </div>
+              {/* Course Title */}
+              <h4>Course Title: {course.title}</h4>
+              <div className="input-group m-bottom">
+                <input type="text" className="form-control" placeholder="Course Title" />
+              </div>
 
-                <hr className="hr" />
-
-                <label>Fall</label>
+              {/* Prereqs */}
+              <h4>Prerequisites:</h4>
+              <div className="input-group">
                 <div className="form-check">
                   <input className="form-check-input" type="checkbox" />
-                  <label className="form-check-label">
-                    Unavailable This Quarter
-                  </label>
+                  <label className="form-check-label">None</label>
                 </div>
-                <div className="input-group m-bottom">
-                  <div className="form-check m-right">
-                    <input className="form-check-input" type="checkbox" />
-                    <label className="form-check-label">M</label>
-                  </div>
-                  <div className="form-check m-right">
-                    <input className="form-check-input" type="checkbox" />
-                    <label className="form-check-label">Tu</label>
-                  </div>
-                  <div className="form-check m-right">
-                    <input className="form-check-input" type="checkbox" />
-                    <label className="form-check-label">W</label>
-                  </div>
-                  <div className="form-check m-right">
-                    <input className="form-check-input" type="checkbox" />
-                    <label className="form-check-label">Th</label>
-                  </div>
-                  <div className="form-check m-right">
-                    <input className="form-check-input" type="checkbox" />
-                    <label className="form-check-label">F</label>
-                  </div>
+              </div>
+              <div className="input-group m-bottom">
+                  {this.state.courses.map((course) => (
+                    <div key={course.id} className="form-check m-right">
+                      <input className="form-check-input" type="checkbox" />
+                      <label className="form-check-label">{course.title}</label>
+                    </div>
+                  ))}
+              </div>
+
+              {/* Fall Quarter */}
+              <h4>Fall</h4>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" />
+                <label className="form-check-label">
+                  Unavailable This Quarter
+                </label>
+              </div>
+              <div className="input-group m-bottom">
+                <div className="form-check m-right">
+                  <input className="form-check-input" type="checkbox" />
+                  <label className="form-check-label">M</label>
                 </div>
-                <div className="input-group">
+                <div className="form-check m-right">
+                  <input className="form-check-input" type="checkbox" />
+                  <label className="form-check-label">Tu</label>
+                </div>
+                <div className="form-check m-right">
+                  <input className="form-check-input" type="checkbox" />
+                  <label className="form-check-label">W</label>
+                </div>
+                <div className="form-check m-right">
+                  <input className="form-check-input" type="checkbox" />
+                  <label className="form-check-label">Th</label>
+                </div>
+                <div className="form-check m-right">
+                  <input className="form-check-input" type="checkbox" />
+                  <label className="form-check-label">F</label>
+                </div>
+              </div>
+
+              {/* Fall Quarter: Times */}
+              {course.times.fall.map((index) => (
+                <React.Fragment>
+                  <div className="input-group">
                   <input type="text" className="form-control" placeholder="Start Time" />
                   <select>
                     <option>AM</option>
                     <option>PM</option>
                   </select>
-                </div>
-                <div className="input-group">
-                  <input type="text" className="form-control" placeholder="End Time" />
-                  <select>
-                    <option>AM</option>
-                    <option>PM</option>
-                  </select>
-                </div>
-                <button className="btn btn-success btn-sm m-top">Add Time</button>
-              </div>
-            ))}
-          </div>
-        ))}
-        <button onClick={this.handleCourse} className="btn btn-primary m-right-sm">Add Course</button>
+                  </div>
+                  <div className="input-group m-bottom">
+                    <input type="text" className="form-control" placeholder="End Time" />
+                    <select>
+                      <option>AM</option>
+                      <option>PM</option>
+                    </select>
+                  </div>
+                </React.Fragment>
+              ))}
+              <button 
+                onClick={() => this.addTime(course.id, 'fall')} 
+                className="btn btn-success btn-sm m-top">
+                  Add Time
+              </button>
+              <button 
+                onClick={() => this.removeTime(course.id, 'fall')} 
+                className="btn btn-warning btn-sm m-top m-left">
+                  Remove Time
+              </button>
+
+              {/* Remove Course */}
+              <br />
+              <button onClick={() => this.removeCourse(course.id)} className="btn btn-danger btn-sm m-top">Remove Course</button>
+              <hr className="hr" />
+            </div>
+          ))}
+        </div>
+       
+        {/* Final Section: Add Courses, Specify Algorithm */}
+        <button onClick={this.addCourse} className="btn btn-primary m-right-sm">Add Course</button>
         <button onClick={this.handleSeries} className="btn btn-primary">Add Year-Long Series</button>
         <div className="form-check m-top-sm">
           <input className="form-check-input" type="checkbox" />
--- src/SampleData.js
+++ src/SampleData.js
@@ -1,159 +1,115 @@
-export const groupsSmall = [
-  [
-    {
-      id: 0, 
-      title: "CS 336",
-      fall: [1300, 1420, [0, 1, 0, 1, 0]],
-      winter: [930, 1050, [0, 1, 0, 1, 0]],
-      spring: [930, 1050, [0, 1, 0, 1, 0]], 
-      series: false,
-      prereq: null
-    }, 
-    {
-      id: 1, 
-      title: "CS 339", 
-      fall: null,
-      winter: [1530, 1650, [0, 1, 0, 1, 0]],
-      spring: [1530, 1650, [0, 1, 0, 1, 0]], 
-      series: false,
-      prereq: null
-    }
-  ]
-];
-
-export const courseIdMap_sm = [
+export const courses_sm = [
   {
-    id: 1,
-    title: "CS 336"
-  },
+    id: 0, 
+    title: "CS 336",
+    fall: [1300, 1420, [0, 1, 0, 1, 0]],
+    winter: [930, 1050, [0, 1, 0, 1, 0]],
+    spring: [930, 1050, [0, 1, 0, 1, 0]], 
+    series: false,
+    prereq: null
+  }, 
   {
-    id: 2,
-    title: "CS 339"
+    id: 1, 
+    title: "CS 339", 
+    fall: null,
+    winter: [1530, 1650, [0, 1, 0, 1, 0]],
+    spring: [1530, 1650, [0, 1, 0, 1, 0]], 
+    series: false,
+    prereq: null
   }
 ];
 
-export const courseIdMap_lg = [
+export const courses_lg = [
   {
-    id: 1,
-    title: "CS 336"
-  },
+    id: 0, 
+    title: "CS 336",
+    fall: [1300, 1420, [0, 1, 0, 1, 0]],
+    winter: [930, 1050, [0, 1, 0, 1, 0]],
+    spring: [930, 1050, [0, 1, 0, 1, 0]], 
+    series: false,
+    prereq: null
+  }, 
   {
-    id: 2,
-    title: "CS 339"
-  },
+    id: 1, 
+    title: "CS 339", 
+    fall: null,
+    winter: [1530, 1650, [0, 1, 0, 1, 0]],
+    spring: [1530, 1650, [0, 1, 0, 1, 0]], 
+    series: false,
+    prereq: null
+  }, 
   {
-    id: 3,
-    title: "CS 348"
-  },
+    id: 2, 
+    title: "CS 348", 
+    fall: [420, 540, [1, 0, 1, 0, 0]],
+    winter: [1400, 1520, [0, 1, 0, 1, 0]],
+    spring: [1230, 1350, [0, 1, 0, 1, 0]], 
+    series: false,
+    prereq: null
+  }, 
   {
-    id: 4,
-    title: "CS 349"
-  },
-  {
-    id: 5,
-    title: "CS 321"
-  },
-  {
-    id: 6,
-    title: "CS 337"
-  }
-];
+    id: 3, 
+    title: "CS 349", 
+    fall: [900, 950, [1, 0, 1, 0, 1]],
+    winter: [1100, 1220, [1, 0, 1, 0, 0]],
+    spring: [1530, 1650, [1, 0, 1, 0, 0]], 
+    series: false,
+    prereq: null
+  }, 
+  
 
-export const groupsLarge = [
-    [
-      {
-        id: 0, 
-        title: "CS 336",
-        fall: [1300, 1420, [0, 1, 0, 1, 0]],
-        winter: [930, 1050, [0, 1, 0, 1, 0]],
-        spring: [930, 1050, [0, 1, 0, 1, 0]], 
-        series: false,
-        prereq: null
-      }, 
-      {
-        id: 1, 
-        title: "CS 339", 
-        fall: null,
-        winter: [1530, 1650, [0, 1, 0, 1, 0]],
-        spring: [1530, 1650, [0, 1, 0, 1, 0]], 
-        series: false,
-        prereq: null
-      }, 
-      {
-        id: 2, 
-        title: "CS 348", 
-        fall: [420, 540, [1, 0, 1, 0, 0]],
-        winter: [1400, 1520, [0, 1, 0, 1, 0]],
-        spring: [1230, 1350, [0, 1, 0, 1, 0]], 
-        series: false,
-        prereq: null
-      }, 
-      {
-        id: 3, 
-        title: "CS 349", 
-        fall: [900, 950, [1, 0, 1, 0, 1]],
-        winter: [1100, 1220, [1, 0, 1, 0, 0]],
-        spring: [1530, 1650, [1, 0, 1, 0, 0]], 
-        series: false,
-        prereq: null
-      }, 
-    ], 
-    [
-      {
-        id: 4, 
-        title: "CS 321", 
-        fall: [1120, 1240, [0, 1, 0, 1, 0]],
-        winter: null,
-        spring: [1120, 1240, [0, 1, 0, 1, 0]], 
-        series: false,
-        prereq: null
-      }, 
-      {
-        id: 5, 
-        title: "CS 330", 
-        fall: [1130, 1220, [1, 0, 1, 0, 1]],
-        winter: null,
-        spring: null,
-        series: false,
-        prereq: null
-      },
-      {
-        id: 6, 
-        title: "CS 337",
-        fall: null,
-        winter: [1530, 1650, [1, 0, 1, 0, 0]],
-        spring: null,
-        series: false,
-        prereq: null
-      }, 
-      {
-        id: 7, 
-        title: "Math 306", 
-        fall: null,
-        winter: [1000, 1050, [1, 0, 1, 0, 1]],
-        spring: null,
-        series: false,
-        prereq: null
-      }
-    ], 
-    [
-      {
-        id: 8, 
-        title: "Math 308", 
-        fall: null,
-        winter: null,
-        spring: [1000, 1050, [1, 0, 1, 0, 1]], 
-        series: false,
-        prereq: null
-      }, 
-      {
-        id: 9, 
-        title: "Math 310 Series", 
-        fall: [[900, 950, [1, 0, 1, 0, 1]], [1100, 1150, [1, 0, 1, 0, 1]]],
-        winter: [[900, 950, [1, 0, 1, 0, 1]], [1100, 1150, [1, 0, 1, 0, 1]]],
-        spring: [[900, 950, [1, 0, 1, 0, 1]], [1100, 1150, [1, 0, 1, 0, 1]]],
-        series: true,
-        prereq: null
-      }
-    ]
+  {
+    id: 4, 
+    title: "CS 321", 
+    fall: [1120, 1240, [0, 1, 0, 1, 0]],
+    winter: null,
+    spring: [1120, 1240, [0, 1, 0, 1, 0]], 
+    series: false,
+    prereq: null
+  }, 
+  {
+    id: 5, 
+    title: "CS 330", 
+    fall: [1130, 1220, [1, 0, 1, 0, 1]],
+    winter: null,
+    spring: null,
+    series: false,
+    prereq: null
+  },
+  {
+    id: 6, 
+    title: "CS 337",
+    fall: null,
+    winter: [1530, 1650, [1, 0, 1, 0, 0]],
+    spring: null,
+    series: false,
+    prereq: null
+  }, 
+  {
+    id: 7, 
+    title: "Math 306", 
+    fall: null,
+    winter: [1000, 1050, [1, 0, 1, 0, 1]],
+    spring: null,
+    series: false,
+    prereq: null
+  },
+  {
+    id: 8, 
+    title: "Math 308", 
+    fall: null,
+    winter: null,
+    spring: [1000, 1050, [1, 0, 1, 0, 1]], 
+    series: false,
+    prereq: null
+  }, 
+  {
+    id: 9, 
+    title: "Math 310 Series", 
+    fall: [[900, 950, [1, 0, 1, 0, 1]], [1100, 1150, [1, 0, 1, 0, 1]]],
+    winter: [[900, 950, [1, 0, 1, 0, 1]], [1100, 1150, [1, 0, 1, 0, 1]]],
+    spring: [[900, 950, [1, 0, 1, 0, 1]], [1100, 1150, [1, 0, 1, 0, 1]]],
+    series: true,
+    prereq: null
+  }
 ];
