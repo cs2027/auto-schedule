d0fee3c1a083ceb88771962c41b5d8a765d34e5a
Christopher Song <chris4728.song2001@gmail.com>
2021-03-24 22:45:00 -0500

    Persist some course data (title, prereqs, etc.) to internal state

--- src/App.js
+++ src/App.js
@@ -6,45 +6,28 @@ import './App.css';
 
 class Input extends Component {
   state = { 
-    currentId: 3,
-    courses: [
-      {
-        id: 1, 
-        title: "Course 1",
-        times: {
-          fall: [{start: null, end: null}],
-          winter: [{start: null, end: null}],
-          spring: [{start: null, end: null}]
-        },
-        series: false,
-        prereq: null
-      }, 
-      {
-        id: 2, 
-        title: "Course 2", 
-        times: {
-          fall: [{start: null, end: null}],
-          winter: [{start: null, end: null}],
-          spring: [{start: null, end: null}]
-        },
-        series: false,
-        prereq: null
-      }
-    ]
+    currentId: 1,
+    courses: []
   }
 
+  ////////////////////////////////
+  // Universal Helper Functions //
+  ////////////////////////////////
+
   // Find course index in internal list, based on its Id number
-  findCourse = (courseId) => {
+  findIndex = (courseId) => {
     let courses = [...this.state.courses];
 
-    for (var i in courses) {
+    for (let i = Math.min(courseId - 1, courses.length - 1); i >= 0; i--) {
       if (courses[i].id === courseId) {
         return i;
       };
     };
-
-    return -1;
   }
+
+  //////////////////////////
+  // Add & remove courses //
+  //////////////////////////
 
   // Add a new course 
   addCourse = () => {
@@ -54,13 +37,28 @@ class Input extends Component {
                 {
                   id: oldState.currentId, 
                   title: `Course ${oldState.currentId}`, 
-                  times: {
-                    fall: [{start: null, end: null}],
-                    winter: [{start: null, end: null}],
-                    spring: [{start: null, end: null}]
+                  lecTimes: {
+                    fall: [{start: null, end: null, dow: [0, 0, 0, 0, 0]}],
+                    winter: [{start: null, end: null, dow: [0, 0, 0, 0, 0]}],
+                    spring: [{start: null, end: null, dow: [0, 0, 0, 0, 0]}]
+                  },
+                  discTimes: {
+                    fall: [{start: null, end: null, dow: [0, 0, 0, 0, 0]}],
+                    winter: [{start: null, end: null, dow: [0, 0, 0, 0, 0]}],
+                    spring: [{start: null, end: null, dow: [0, 0, 0, 0, 0]}]
+                  },
+                  available: {
+                    fall: true,
+                    winter: true,
+                    spring: true
+                  },
+                  disc: {
+                    fall: true,
+                    winter: true,
+                    spring: true
                   },
                   series: false,
-                  prereq: null
+                  prereqs: []
                 }]
     }));
   };
@@ -72,43 +70,139 @@ class Input extends Component {
     }));
   };
 
-  // Add a time slot for a course (ex. 9am and 11am sections)
-  addTime = (courseId, quarter) => {
-    let index = this.findCourse(courseId);
+  // Method to add year-long course sequences (TODO)
+  handleSeries = () => {
+    console.log('Handle Series');
+  };
 
-    if (index === -1) {
-      return;
-    }
+  //////////////////////////
+  // Update lecture times //
+  //////////////////////////
+
+  // Add a lecture time slot for a course (ex. 9am and 11am sections)
+  addLecture = (courseId, quarter) => {
+    let index = this.findIndex(courseId);
 
     let courses = [...this.state.courses];
-    courses[index].times[quarter].push({start: null, end: null});
+    courses[index].lecTimes[quarter].push({start: null, end: null, dow: [0, 0, 0, 0, 0]});
     this.setState({ courses });
   };
 
-  // Remove a time slot for a course
-  removeTime = (courseId, quarter) => {
-    let index = this.findCourse(courseId);
-
-    if (index === -1) {
-      return;
-    }
+  // Remove a lecture time slot for a course
+  removeLecture = (courseId, quarter) => {
+    let index = this.findIndex(courseId);
 
     let courses = [...this.state.courses];
-    if (courses[index].times[quarter].length === 1) {
+    if (courses[index].lecTimes[quarter].length === 1) {
       alert('Error: You are attempting to remove the only time slot.');
       return;
     };
 
-    courses[index].times[quarter].pop();
+    courses[index].lecTimes[quarter].pop();
     this.setState({ courses });
   };
 
-  // TODO
-  handleSeries = () => {
-    console.log('Handle Series');
+  /////////////////////////////
+  // Update discussion times //
+  /////////////////////////////
+
+  // Add a discussion section time slot for a course
+  addDiscussion = (courseId, quarter) => {
+    let index = this.findIndex(courseId);
+
+    let courses = [...this.state.courses];
+    courses[index].discTimes[quarter].push({start: null, end: null, dow: [0, 0, 0, 0, 0]});
+    this.setState({ courses });
+  };
+
+  // Remove a discussion section time slot for a course
+  removeDiscussion = (courseId, quarter) => {
+    let index = this.findIndex(courseId);
+
+    let courses = [...this.state.courses];
+    if (courses[index].discTimes[quarter].length === 1) {
+      alert("Error: Please select the 'No Discussion Section' box instead.");
+      return;
+    };
+
+    courses[index].discTimes[quarter].pop();
+    this.setState({ courses });
+  };
+
+  //////////////////
+  // Course title //
+  //////////////////
+
+  // Update course title
+  changeTitle = ({target}) => {
+    let index = this.findIndex(parseInt(target.id));
+
+    let courses = [...this.state.courses];
+    courses[index].title = target.value;
+    this.setState({ courses });
+  };
+
+  ////////////////////
+  // Course prereqs //
+  ////////////////////
+
+  // Update whether or not a course has prereqs
+  prereqStatus = ({target}) => {
+    let index = this.findIndex(parseInt(target.id));
+    let courses = [...this.state.courses];
+
+    if (courses[index].prereqs === null) {
+      courses[index].prereqs = [];
+    } else {
+      courses[index].prereqs = null;
+    }
+
+    this.setState({ courses });
   }
 
-  // Render method
+  // Add or remove a prereq for a course
+  editPrereqs = (courseId, prereqId) => {
+    let index = this.findIndex(courseId);
+    let courses = [...this.state.courses];
+  
+    if (courses[index].prereqs.includes(prereqId)) {
+      let prereqs = courses[index].prereqs;
+      courses[index].prereqs = prereqs.filter(Id => Id !== prereqId);
+    } else {
+      courses[index].prereqs.push(prereqId);
+    }
+
+    this.setState({ courses });
+  };
+
+  /////////////////////////
+  // Course availability //
+  /////////////////////////
+
+  // Update whether or not a course is available in a given quarter
+  changeAvail = (courseId, quarter) => {
+    let index = this.findIndex(courseId);
+    let courses = [...this.state.courses];
+
+    let avail = courses[index].available[quarter];
+    courses[index].available[quarter] = !(avail);
+    this.setState({ courses });
+  };
+
+  // Update whether or not a course has a disc. section for a given quarter
+  changeDisc = (courseId, quarter) => {
+    let index = this.findIndex(courseId);
+    let courses = [...this.state.courses];
+
+    let status = courses[index].disc[quarter];
+    courses[index].disc[quarter] = !(status); 
+    this.setState({ courses });
+  };
+
+  ///////////////////
+  // Render method //
+  ///////////////////
+
   render() { 
     return ( 
       <div className="m-bottom">
@@ -119,89 +213,184 @@ class Input extends Component {
               {/* Course Title */}
               <h4>Course Title: {course.title}</h4>
               <div className="input-group m-bottom">
-                <input type="text" className="form-control" placeholder="Course Title" />
+                <input 
+                  onChange={this.changeTitle} 
+                  id={course.id}
+                  value={course.title}
+                  type="text"
+                  className="form-control" 
+                />
               </div>
 
               {/* Prereqs */}
               <h4>Prerequisites:</h4>
               <div className="input-group">
                 <div className="form-check">
-                  <input className="form-check-input" type="checkbox" />
+                  <input 
+                    onChange={this.prereqStatus}
+                    checked={this.state.courses[this.findIndex(course.id)].prereqs === null} 
+                    id={course.id}
+                    className="form-check-input" 
+                    type="checkbox" 
+                  />
                   <label className="form-check-label">None</label>
                 </div>
               </div>
-              <div className="input-group m-bottom">
-                  {this.state.courses.map((course) => (
-                    <div key={course.id} className="form-check m-right">
-                      <input className="form-check-input" type="checkbox" />
-                      <label className="form-check-label">{course.title}</label>
+              <div 
+                hidden={this.state.courses[this.findIndex(course.id)].prereqs === null} 
+                className="input-group"
+              >
+                  {this.state.courses.map((prereq) => (
+                    <div key={prereq.id} className="form-check m-right">
+                      <input 
+                        onChange={() => this.editPrereqs(course.id, prereq.id)}
+                        checked={this.state.courses[this.findIndex(course.id)].prereqs !== null &&
+                                this.state.courses[this.findIndex(course.id)].prereqs.includes(prereq.id)}
+                        className="form-check-input" 
+                        type="checkbox" 
+                      />
+                      <label className="form-check-label">{prereq.title}</label>
                     </div>
                   ))}
               </div>
 
-              {/* Fall Quarter */}
-              <h4>Fall</h4>
+              {/* START: Fall Quarter */}
+              <h4 className="m-top">Fall</h4>
               <div className="form-check">
-                <input className="form-check-input" type="checkbox" />
+                <input 
+                  onChange={() => this.changeAvail(course.id, 'fall')}
+                  checked={!(this.state.courses[this.findIndex(course.id)].available.fall)}
+                  className="form-check-input" 
+                  type="checkbox" 
+                />
                 <label className="form-check-label">
                   Unavailable This Quarter
                 </label>
               </div>
-              <div className="input-group m-bottom">
-                <div className="form-check m-right">
-                  <input className="form-check-input" type="checkbox" />
-                  <label className="form-check-label">M</label>
-                </div>
-                <div className="form-check m-right">
-                  <input className="form-check-input" type="checkbox" />
-                  <label className="form-check-label">Tu</label>
-                </div>
-                <div className="form-check m-right">
-                  <input className="form-check-input" type="checkbox" />
-                  <label className="form-check-label">W</label>
-                </div>
-                <div className="form-check m-right">
-                  <input className="form-check-input" type="checkbox" />
-                  <label className="form-check-label">Th</label>
-                </div>
-                <div className="form-check m-right">
-                  <input className="form-check-input" type="checkbox" />
-                  <label className="form-check-label">F</label>
-                </div>
+              <div className="form-check">
+                <input 
+                  onChange = {() => this.changeDisc(course.id, 'fall')}
+                  checked={!(this.state.courses[this.findIndex(course.id)].disc.fall)}
+                  className="form-check-input" 
+                  type="checkbox" 
+                />
+                <label className="form-check-label">
+                  No Discussion Section
+                </label>
               </div>
 
-              {/* Fall Quarter: Times */}
-              {course.times.fall.map((index) => (
-                <React.Fragment>
-                  <div className="input-group">
-                  <input type="text" className="form-control" placeholder="Start Time" />
-                  <select>
-                    <option>AM</option>
-                    <option>PM</option>
-                  </select>
-                  </div>
-                  <div className="input-group m-bottom">
-                    <input type="text" className="form-control" placeholder="End Time" />
+              <div hidden={!(this.state.courses[this.findIndex(course.id)].available.fall)}>
+                {/* Fall Quarter: Lecture Times */}
+                <h5 className="m-top-lg">Lectures</h5>
+                {course.lecTimes.fall.map((index) => (
+                  <React.Fragment>
+                    <div className="input-group m-bottom">
+                      <div className="form-check m-right">
+                        <input className="form-check-input" type="checkbox" />
+                        <label className="form-check-label">M</label>
+                      </div>
+                      <div className="form-check m-right">
+                        <input className="form-check-input" type="checkbox" />
+                        <label className="form-check-label">Tu</label>
+                      </div>
+                      <div className="form-check m-right">
+                        <input className="form-check-input" type="checkbox" />
+                        <label className="form-check-label">W</label>
+                      </div>
+                      <div className="form-check m-right">
+                        <input className="form-check-input" type="checkbox" />
+                        <label className="form-check-label">Th</label>
+                      </div>
+                      <div className="form-check m-right">
+                        <input className="form-check-input" type="checkbox" />
+                        <label className="form-check-label">F</label>
+                      </div>
+                    </div>
+                    <div className="input-group">
+                    <input type="text" className="form-control" placeholder="Start Time" />
                     <select>
                       <option>AM</option>
                       <option>PM</option>
                     </select>
-                  </div>
-                </React.Fragment>
-              ))}
-              <button 
-                onClick={() => this.addTime(course.id, 'fall')} 
-                className="btn btn-success btn-sm m-top">
-                  Add Time
-              </button>
-              <button 
-                onClick={() => this.removeTime(course.id, 'fall')} 
-                className="btn btn-warning btn-sm m-top m-left">
-                  Remove Time
-              </button>
+                    </div>
+                    <div className="input-group m-bottom">
+                      <input type="text" className="form-control" placeholder="End Time" />
+                      <select>
+                        <option>AM</option>
+                        <option>PM</option>
+                      </select>
+                    </div>
+                  </React.Fragment>
+                ))}
+                <button 
+                  onClick={() => this.addLecture(course.id, 'fall')} 
+                  className="btn btn-success btn-sm m-top">
+                    Add Lecture Time
+                </button>
+                <button 
+                  onClick={() => this.removeLecture(course.id, 'fall')} 
+                  className="btn btn-warning btn-sm m-top m-left">
+                    Remove Lecture Time
+                </button>
+
+                {/* Fall Quarter: Discussion Times */}
+                <div hidden={!(this.state.courses[this.findIndex(course.id)].disc.fall)}>
+                  <h5 className="m-top-lg">Discussion Section</h5>
+                  {course.discTimes.fall.map((index) => (
+                    <React.Fragment>
+                      <div className="input-group m-bottom">
+                        <div className="form-check m-right">
+                          <input className="form-check-input" type="checkbox" />
+                          <label className="form-check-label">M</label>
+                        </div>
+                        <div className="form-check m-right">
+                          <input className="form-check-input" type="checkbox" />
+                          <label className="form-check-label">Tu</label>
+                        </div>
+                        <div className="form-check m-right">
+                          <input className="form-check-input" type="checkbox" />
+                          <label className="form-check-label">W</label>
+                        </div>
+                        <div className="form-check m-right">
+                          <input className="form-check-input" type="checkbox" />
+                          <label className="form-check-label">Th</label>
+                        </div>
+                        <div className="form-check m-right">
+                          <input className="form-check-input" type="checkbox" />
+                          <label className="form-check-label">F</label>
+                        </div>
+                      </div>
+                      <div className="input-group">
+                      <input type="text" className="form-control" placeholder="Start Time" />
+                      <select>
+                        <option>AM</option>
+                        <option>PM</option>
+                      </select>
+                      </div>
+                      <div className="input-group m-bottom">
+                        <input type="text" className="form-control" placeholder="End Time" />
+                        <select>
+                          <option>AM</option>
+                          <option>PM</option>
+                        </select>
+                      </div>
+                    </React.Fragment>
+                  ))}
+                  <button 
+                    onClick={() => this.addDiscussion(course.id, 'fall')} 
+                    className="btn btn-success btn-sm m-top">
+                      Add Disc. Time
+                  </button>
+                  <button 
+                    onClick={() => this.removeDiscussion(course.id, 'fall')} 
+                    className="btn btn-warning btn-sm m-top m-left">
+                      Remove Disc. Time
+                  </button>
+                </div>
+              </div>
+              {/* END: Fall Quarter */}
 
               {/* Remove Course */}
-              <br />
               <button onClick={() => this.removeCourse(course.id)} className="btn btn-danger btn-sm m-top">Remove Course</button>
               <hr className="hr" />
             </div>
